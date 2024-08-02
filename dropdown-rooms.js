5a9932e6d22c60a715ddf11dd67215b72b9d584c
asheralieva <84007297+asheralieva@users.noreply.github.com>
2024-08-02 15:14:28 +0600

    swiper trying

--- dropdown-rooms.js
+++ dropdown-rooms.js
@@ -21,3 +21,4 @@ dropdowns.forEach(dropdown => {
         });
     });
 });
+
---
+++ slider.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.double-room-swiper', {
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,  
+    },
+    slidesPerView: 1,
+    spaceBetween: 30,
+  });
