4cbb7acb452d83a12acc110d72e20e1e5f0b4725
Nurtenir <nurtenir@example.com>
2024-08-05 19:33:46 +0600

    create burger-menu

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const menuIcon = document.getElementById('menu-icon')
+  const navLinks = document.querySelector('.nav-links')
+
+  menuIcon.addEventListener('click', () => {
+    navLinks.classList.toggle('open')
+  })
+})
 const swiper = new Swiper('.swiper', {
 	loop: true,
 
