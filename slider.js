32a8b0e80a31600fc36588d5dc3474b73c174485
asheralieva <84007297+asheralieva@users.noreply.github.com>
2024-08-07 00:18:33 +0600

    fff

--- slider.js
+++
@@ -1,9 +0,0 @@
-const swiper = new Swiper('.double-room-swiper', {
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    slidesPerView: 1,
-    spaceBetween: 30,
-  });
-
